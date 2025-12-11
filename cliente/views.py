a997cd0f7eef3abeb489c725ce85b20882e33e4a
EliasdsAI <jedantas64@gmail.com>
2025-12-10 21:37:56 -0300

    Add files via upload

--- cliente/views.py
+++ cliente/views.py
@@ -1,14 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from models import Cliente
 def cadastrar_cliente(request):
     if request.method == 'POST':
-         codcli = request.POST.get('codcli')
          nome = request.POST.get('nome')
          telefone = request.POST.get('telefone')
-         cliente = Cliente(codcli, nome, telefone)
-         return HttpResponse(f"Cliente cadastrado com sucesso! <br> Código: "
-                            f"{cliente.codcli} <br> Nome: {cliente.nome} "
-                            f"<br> Telefone: {cliente.telefone}")
-         return render(request, 'formcliente.html')
+         email = request.POST.get('email')
+
+         cliente = Cliente(nome, telefone, email)
+         return HttpResponse(f"Cliente cadastrado com sucesso! <br>: "
+                            f"<br> Nome: {cliente.nome} "
+                            f"<br> Telefone: {cliente.telefone}"
+                            f"<br> Email: {cliente.email} ")
+         return render(request, 'cadastro_cliente.html')
